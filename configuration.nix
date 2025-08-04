35f8e35562e6a95fcf63c0e7bf372b0a2d87155b
Ahmed Khanzada <me@enzu.ru>
2025-08-04 09:38:08 -0700

    Cleaning up config

--- configuration.nix
+++ configuration.nix
@@ -6,33 +6,20 @@
 
 {
   imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
+    [
+      /etc/nixos/hardware-configuration.nix
     ];
 
-  # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-
-  # Use latest kernel.
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
-  networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
+  networking.hostName = "nixos";
   networking.networkmanager.enable = true;
 
-  # Set your time zone.
   time.timeZone = "America/Los_Angeles";
 
-  # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
-
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "en_US.UTF-8";
     LC_IDENTIFICATION = "en_US.UTF-8";
@@ -45,50 +32,32 @@
     LC_TIME = "en_US.UTF-8";
   };
 
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-
-  # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
-
-  # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "us";
-    variant = "";
-  };
-
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-
-  # Enable sound with pipewire.
-  services.pulseaudio.enable = false;
-  security.rtkit.enable = true;
+  services.flatpak.enable = true;
+  services.libinput.enable = true;
+  services.openssh.enable = true;
   services.pipewire = {
     enable = true;
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
   };
-
-  services.flatpak.enable = true;
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
+  services.printing.enable = true;
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.xserver.enable = true;
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
 
   fonts.packages = with pkgs; [
     ipafont
     hanazono
+    noto-fonts
   ];
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  # programs.fish.enable = true;
   users.users.enzuru = {
     isNormalUser = true;
     description = "Ahmed Khanzada";
@@ -101,43 +70,41 @@
       emacs-pgtk
       flatpak-builder
       fractal
-      gettext
       gimp
       glances
       gnugo
       gnumake
       gnome-builder
       inkscape
-      meson
       mc
-      ninja
       nodejs
-      noto-fonts
       obsidian
-      pantheon.granite7
       sbcl
       thunderbird
       tree-sitter
-      vala-language-server
-      vala
     ];
   };
 
-  # Install firefox.
   programs.firefox.enable = true;
   programs.fish.enable = true;
 
-  # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
   environment.systemPackages = with pkgs; [
     curl
     git
     mg
     gnomeExtensions.paperwm
   ];
+
+  systemd = {
+    targets = {
+      sleep = { enable = false; unitConfig.DefaultDependencies = "no"; };
+      suspend = { enable = false; unitConfig.DefaultDependencies = "no"; };
+      hibernate = { enable = false; unitConfig.DefaultDependencies = "no"; };
+      "hybrid-sleep" = { enable = false; unitConfig.DefaultDependencies = "no"; };
+    };
+  };
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
@@ -150,7 +117,6 @@
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
@@ -165,5 +131,4 @@
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "25.05"; # Did you read the comment?
-
 }
