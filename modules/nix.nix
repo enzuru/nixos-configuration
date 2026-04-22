3d115a45cf487e69d5ad8c4e6cec47edbc9443e2
Ahmed Khanzada <me@enzu.ru>
2026-04-22 14:27:33 -0700

    Add some garbage collection and optimization

--- modules/nix.nix
+++ modules/nix.nix
@@ -1,7 +1,13 @@
 { config, pkgs, ... }:
 
 {
-  nixpkgs.config.allowUnfree = true;
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 30d";
+  };
+
+  nix.optimise.automatic = true;
 
   nix.settings = {
     substituters = [
@@ -13,4 +19,6 @@
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
   };
+
+  nixpkgs.config.allowUnfree = true;
 }
